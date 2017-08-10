58e5ea21f64419173ee84c8e2684a661e98598da
ProHiryu <songheqi1996@gmail.com>
2017-08-10 21:58:11 +0800

    get names

---
+++ code/data_retreiving/get_image.py
@@ -0,0 +1,5 @@
+# -*-coding:utf8-*-
+
+from bs4 import BeautifulSoup
+import requests
+from get_names import get_name_list
--- code/data_retreiving/get_names.py
+++ code/data_retreiving/get_names.py
@@ -3,10 +3,36 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://zh.wikipedia.org/wiki/AV%E5%A5%B3%E5%84%AA%E5%88%97%E8%A1%A8#2016.E5.B9.B4.E5.87.BA.E9.81.93_AV.E5.A5.B3.E5.84.AA.E5.88.97.E8.A1.A8"
 
-r = requests.get(url)
+def get_name_list():
+    url = "http://www.ttpaihang.com/vote/rank.php?voteid=1089"
+    page = "&page="
 
-soup = BeautifulSoup(r,"html.parser")
+    Name_List = []
 
-print(soup)
+    for i in range(5):
+        i += 1
+        i = str(i)
+        page_use = page + i
+        url_use = url + page_use
+
+        r = requests.get(url_use)
+        r.encoding = 'gbk'
+
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.text,"html.parser")
+        else:
+            print("requests error")
+
+        ztheis = soup.find_all('td',class_ = "zthei")
+
+        for zthei in ztheis:
+            if zthei.find_all("a") != []:
+                a = zthei.find("a")
+                Name_List.append(a.contents[0])
+
+    return Name_List
+
+if __name__ == "__main__":
+    r = get_name_list()
+    print(r)
