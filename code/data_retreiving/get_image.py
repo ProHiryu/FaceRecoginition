77e4400840281164cfc03fa2f532ae43c224d434
ProHiryu <songheqi1996@gmail.com>
2017-08-12 22:59:37 +0800

    get names

--- code/data_retreiving/get_image.py
+++ code/data_retreiving/get_image.py
@@ -1,5 +1,11 @@
 # -*-coding:utf8-*-
 
-from bs4 import BeautifulSoup
 import requests
 from get_names import get_name_list
+
+key = "AIzaSyDDC5orS4QQiXcKbQSU60kDgQ_VOML-2bc"
+
+def get_image():
+    names = get_name_list()
+    for name in names:
+        re = url_open(url,name)
